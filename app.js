c619176408aaf9940a654951c899d5d37041386f
eshan1925 <eshan1925@gmail.com>
2022-01-06 00:37:47 +0530

    Enabled fetching and storing of blog posts

--- app.js
+++ app.js
@@ -9,35 +9,40 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+var posts= [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",(function(req,res){
-  res.render("home",{startingContent:homeStartingContent});
-}));
+app.get("/",function(req,res){
+  res.render("home",{
+    startingContent:homeStartingContent,
+    posts:posts
+  });
+});
 
-app.get("/about",(function(req,res){
+app.get("/about",function(req,res){
   res.render("about",{startingContent:aboutContent});
-}));
+});
 
-app.get("/contact",(function(req,res){
+app.get("/contact",function(req,res){
   res.render("contact",{startingContent:contactContent});
-}));
+});
 
 
-app.get("/compose",(function(req,res){
+app.get("/compose",function(req,res){
   res.render("compose");
-}));
+});
 
-
-
-
-
-
-
+app.post("/compose",function(req,res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
