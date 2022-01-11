f1ca276d62cc82bd193f29b2228f7712345f403e
eshan1925 <eshan1925@gmail.com>
2022-01-11 15:35:53 +0530

    Added and Hosted DB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +17,31 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//connecting or creating of database to server
+mongoose.connect("mongodb+srv://admin-eshan:Test123@cluster0.l312f.mongodb.net/BlogsDB", { useNewUrlParser: true });
+
+// connecting or creating of database to local server
+// mongoose.connect("mongodb://localhost:27017/BlogsDB");
+
+var blogPostSchema = {
+  title: String,
+  content: String
+};
+
+var blogPost = mongoose.model("post", blogPostSchema);
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  blogPost.find({}, function (err, foundPosts) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully found Items!!!");
+      console.log(foundPosts);
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: foundPosts
+      });
+    }
   });
 });
 
@@ -32,17 +54,17 @@ app.get("/contact", function (req, res) {
 });
 
 
-app.get("/compose", function (req, res) {
+app.get("/composeYashraj25@", function (req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = lodash.lowerCase(req.params.postName);
-  console.log(req.params.postName);
-  posts.forEach(post => {
-    const storedTitle = lodash.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post", { title: post.title, content: post.content });
+app.get("/posts/:postId", function (req, res) {
+  const requestedId = req.params.postId;
+  blogPost.findOne({_id:requestedId}, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", { title: foundPost.title, content: foundPost.content });
     }
   });
 });
@@ -53,7 +75,17 @@ app.post("/compose", function (req, res) {
     content: req.body.postBody
   };
   posts.push(post);
-  res.redirect("/");
+
+  var blogPost1 = new blogPost({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  blogPost1.save(function (err) {
+    if (!err) {
+      console.log("Successfully added blog post to DB");
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function () {
