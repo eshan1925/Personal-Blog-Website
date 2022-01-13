1cc3b72040fbcda69f3e23d4df08dbf927c2ec27
eshan1925 <eshan1925@gmail.com>
2022-01-13 11:13:25 +0530

    Minor Bug FIxes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
 const mongoose = require("mongoose");
+const _env = require('dotenv').config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,10 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //connecting or creating of database to server
-mongoose.connect("mongodb+srv://admin-eshan:Test123@cluster0.l312f.mongodb.net/BlogsDB", { useNewUrlParser: true });
+// mongoose.connect("mongodb+srv://admin-eshan:Test123@cluster0.l312f.mongodb.net/BlogsDB", { useNewUrlParser: true });
 
 // connecting or creating of database to local server
-// mongoose.connect("mongodb://localhost:27017/BlogsDB");
+mongoose.connect("mongodb://localhost:27017/BlogsDB");
 
 var blogPostSchema = {
   title: String,
@@ -54,7 +55,7 @@ app.get("/contact", function (req, res) {
 });
 
 
-app.get("/composeYashraj25@", function (req, res) {
+app.get("/compose"+process.env.COMPOSE_PASSWORD, function (req, res) {
   res.render("compose");
 });
 
